8f86c63a62f145c3908072ef7cc2252a6c7cce1e
Aayush Kumar Thakur <thakuraayush1999@gmail.com>
2020-06-06 20:20:33 +0530

    SecondAct

--- app/src/main/java/com/example/daero/MainActivity.kt
+++ app/src/main/java/com/example/daero/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.daero
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             Log.i("MainActivity", "Button was clicked !")
 
             Toast.makeText(this,"Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message , Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
 
 
