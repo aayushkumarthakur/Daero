84fb90ef5adeef074ec77c826c50760c75cc7919
Aayush Kumar Thakur <thakuraayush1999@gmail.com>
2020-06-06 19:11:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/daero/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.daero
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was clicked !")
+
+            Toast.makeText(this,"Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+}
